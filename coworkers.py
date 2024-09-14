fc419175ba80b9fea542572ed844695e7f7f4d54
Polina <polinusechka@mail.ru>
2024-09-14 15:15:57 +0300

    coworkers.py

--- coworkers.py
+++ coworkers.py
@@ -1,1 +1,16 @@
+coworkers = ['Иван', 'Катя', 'Саша', 'Юля', 'Витя']
+print(coworkers[0])
+print(coworkers[-1])
+print(coworkers[:])
+print(coworkers[1::2])
+print(len(coworkers))
 
+new_coworcker = input('Введите имя: ')
+coworkers.append(new_coworcker)
+print(len(coworkers))
+
+coworcker = input('Введите имя: ')
+if coworcker in coworkers:
+    print('Имя есть в списке')
+else:
+    print('Имени нет в списке')
