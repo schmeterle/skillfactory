1278eaac626186f7e0d87a76c208856afc53fd0b
Polina <polinusechka@mail.ru>
2024-09-14 16:10:01 +0300

    todo.py

---
+++ todo.py
@@ -0,0 +1,15 @@
+n = int(input('Введите кол-во дел на сегодня: '))
+todo = []
+print('Введите задачи на сегодня: ')
+for i in range(n) :
+    task = input(f'{i+1}')
+    todo.append(task)
+print('Список дел на сегодня: ')
+print(todo)
+n = int(input('Введите номер дела, которое нужно отредактировать: '))
+task = input('Введите новое описание для дела: ')
+todo[n-1] = task
+n = int(input('Введите индекс дела, которое нужно удалить: '))
+todo.pop(n)
+print('Список дел на сегодня: ')
+print(todo)
